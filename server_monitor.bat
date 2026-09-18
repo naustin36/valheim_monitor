7bd06f103bdd2ff224d5e28f937c22d4bfc8ae3c
Nathan Austin <n_austin363@proton.me>
2026-09-18 08:50:15 -0600

    added .bat for starting GUI

---
+++ server_monitor.bat
@@ -0,0 +1,8 @@
+@echo off
+
+echo "Starting Valheim Crossplay Server Monitor"
+echo "This console provides a log of important and useful events"
+echo "To quit, close the monitor GUI window. This ensures proper closing of log file handle"
+
+
+python monitor_gui.py
